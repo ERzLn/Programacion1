546bc402c61a80436f86b0b7ddc5029b5bc0581e
ERzLn <114443743+ERzLn@users.noreply.github.com>
2023-03-05 23:08:52 +0100

    Add files via upload

---
+++ Unidad_5/Hoja 10/u5Hoja10Ej1/src/main/java/hoja10/u5hoja10/Griego.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja10.u5hoja10;
+
+/**
+ *
+ * @author DAW122
+ */
+public class Griego extends Guerrero {
+
+    public Griego(String NOMBRE, int edad, int fuerza) {
+        super(NOMBRE, edad, fuerza);
+    }
+    public Griego(){
+        Teclado.tecladoGuerrero();
+    }
+
+    @Override
+    public boolean retirarse() {
+        boolean retirarse = false;
+        if (super.isHerido()&&!super.isMuerto()) {
+            retirarse = true;
+        }
+        return retirarse;
+    }
+}
