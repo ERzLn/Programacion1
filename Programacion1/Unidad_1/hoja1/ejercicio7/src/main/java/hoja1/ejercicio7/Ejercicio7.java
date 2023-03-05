8430abb3f1af1dbb354154789b2bb86ac499ae8a
Eloy <Eloy@PC>
2023-03-05 22:35:31 +0100

    Merge origin/main

---
+++ Programacion1/Unidad_1/hoja1/ejercicio7/src/main/java/hoja1/ejercicio7/Ejercicio7.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package hoja1.ejercicio7;
+
+/**
+ *
+ * @author xboxr
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+int numero=56;
+        boolean par=(numero%2==0); 
+        System.out.println("Si es true el número es par "+ par);
+    }
+}
