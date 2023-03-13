9c2313a20db6f543dace7acd23b3d53e888c3245
ERzLn <eloy.ruizlinares@iesmiguelherrero.com>
2023-03-13 15:25:11 +0100

    Unidad 6

--- Unidad_6/Hoja 2/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1Hoja2.java
+++ Unidad_6/Hoja 2/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1Hoja2.java
@@ -4,6 +4,7 @@ package com.mycompany.ejercicio1;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -19,11 +20,20 @@ public class Ejercicio1Hoja2 {
     public static ArrayList<Integer> leeValores() {
         ArrayList<Integer> listaValores = new ArrayList<>();
         System.out.println("Introduce valores numéricos enteros (termina con -99):");
-        int valor = new Scanner(System.in).nextInt();
+        boolean salir=false;
+        while (!salir){
+       try{ int valor = new Scanner(System.in).nextInt();
         while (valor != -99) {
             listaValores.add(valor);
             valor = new Scanner(System.in).nextInt();
-        }
+            salir=true;
+            System.out.println("Numero añadido");
+        }}
+       catch (InputMismatchException e){
+           System.out.println("Formato numero entero incorrecto, vuelve a intentarlo");
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }}
         return listaValores;
     }
 
@@ -37,10 +47,10 @@ public class Ejercicio1Hoja2 {
         return suma;
     }
 
-    public static void mostrarResultados(ArrayList<Integer> listaValores, int suma,
-            double media) {
+    public static void mostrarResultados(ArrayList<Integer> listaValores, int suma, double media) {
         System.out.println("Números introducidos: " + listaValores.size());
         System.out.println("Suma: " + suma);
+        System.out.println("Los numeros introducidos son: "+listaValores.toString());
         System.out.printf("La media es: %,.2f\n", media );
         String valoresEncimaMedia = "";
         for (Integer listaValore : listaValores) {
